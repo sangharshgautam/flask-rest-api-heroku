17b352c04caa18f8002155f80cd1e9c3d79eb3f9
Sangharsh Gautam <sangharsh_iitd@yahoo.com>
2019-05-03 16:53:51 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, send_from_directory, render_template, send_file
+from flask import Flask
 from flask_restful import Resource, reqparse, Api
 
 app = Flask(__name__)
@@ -12,22 +12,18 @@ db.init_app(app)
 app.app_context().push()
 db.create_all()
 
-@app.route('/upload')
-def upload_html():
-	return render_template('upload.html')
-
 class Movies_List(Resource):
     parser = reqparse.RequestParser()
     parser.add_argument('director', type=str, required=False, help='Director of the movie')
     parser.add_argument('genre', type=str, required=False, help='Genre of the movie')
     parser.add_argument('collection', type=int, required=True, help='Gross collection of the movie')
-    
+
     def get(self, movie):
         item = Movies.find_by_title(movie)
         if item:
             return item.json()
         return {'Message': 'Movie is not found'}
-    
+
     def post(self, movie):
         if Movies.find_by_title(movie):
             return {' Message': 'Movie with the  title {} already exists'.format(movie)}
@@ -35,7 +31,7 @@ class Movies_List(Resource):
         item = Movies(movie, args['director'], args['genre'], args['collection'])
         item.save_to()
         return item.json()
-        
+
     def put(self, movie):
         args = Movies_List.parser.parse_args()
         item = Movies.find_by_title(movie)
@@ -46,21 +42,21 @@ class Movies_List(Resource):
         item = Movies(movie, args['director'], args['genre'], args['collection'])
         item.save_to()
         return item.json()
-            
+
     def delete(self, movie):
         item  = Movies.find_by_title(movie)
         if item:
             item.delete_()
             return {'Message': '{} has been deleted from records'.format(movie)}
         return {'Message': '{} is already not on the list'.format()}
-    
+
 class All_Movies(Resource):
     def get(self):
         return {'Movies': list(map(lambda x: x.json(), Movies.query.all()))}
 
-api.add_resource(All_Movies, '/movies')
+api.add_resource(All_Movies, '/')
 api.add_resource(Movies_List, '/<string:movie>')
 
 if __name__=='__main__':
-    
-app.run(debug=True)
+
+    app.run(debug=True)
