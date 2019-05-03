cf536009267cc10fefd1703b7a6c427898f4a74d
Sangharsh Gautam <sangharsh_iitd@yahoo.com>
2019-05-03 13:56:14 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, send_from_directory, render_template, send_file
 from flask_restful import Resource, reqparse, Api
 
 app = Flask(__name__)
@@ -11,6 +11,10 @@ from base import Movies, db
 db.init_app(app)
 app.app_context().push()
 db.create_all()
+
+@application.route('/upload')
+def upload_html():
+	return render_template('upload.html')
 
 class Movies_List(Resource):
     parser = reqparse.RequestParser()
