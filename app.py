8c275f531848eb0fa92761e063685117b338f504
Sangharsh Gautam <sangharsh_iitd@yahoo.com>
2019-05-14 13:13:28 +0000

    camera

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, Response
 from flask_restful import Resource, reqparse, Api
 from camera import Camera
+from base import Movies, db
+from movies import MoviesList
 
 app = Flask(__name__)
 api = Api(app)
@@ -8,7 +10,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///base.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['PROPAGATE_EXCEPTIONS'] = True
 
-from base import Movies, db
+
 db.init_app(app)
 app.app_context().push()
 db.create_all()
@@ -29,52 +31,18 @@ def video_feed():
     return Response(gen(Camera()),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
-class Movies_List(Resource):
-    parser = reqparse.RequestParser()
-    parser.add_argument('director', type=str, required=False, help='Director of the movie')
-    parser.add_argument('genre', type=str, required=False, help='Genre of the movie')
-    parser.add_argument('collection', type=int, required=True, help='Gross collection of the movie')
+@app.route('/home')
+def home():
+	return render_template('test_camera.html')
 
-    def get(self, movie):
-        item = Movies.find_by_title(movie)
-        if item:
-            return item.json()
-        return {'Message': 'Movie is not found'}
+class AllMovies(Resource):
 
-    def post(self, movie):
-        if Movies.find_by_title(movie):
-            return {' Message': 'Movie with the  title {} already exists'.format(movie)}
-        args = Movies_List.parser.parse_args()
-        item = Movies(movie, args['director'], args['genre'], args['collection'])
-        item.save_to()
-        return item.json()
-
-    def put(self, movie):
-        args = Movies_List.parser.parse_args()
-        item = Movies.find_by_title(movie)
-        if item:
-            item.collection = args['collection']
-            item.save_to()
-            return {'Movie': item.json()}
-        item = Movies(movie, args['director'], args['genre'], args['collection'])
-        item.save_to()
-        return item.json()
-
-    def delete(self, movie):
-        item  = Movies.find_by_title(movie)
-        if item:
-            item.delete_()
-            return {'Message': '{} has been deleted from records'.format(movie)}
-        return {'Message': '{} is already not on the list'.format()}
-
-
-class All_Movies(Resource):
     def get(self):
         return {'Movies': list(map(lambda x: x.json(), Movies.query.all()))}
 
 
-api.add_resource(All_Movies, '/movies')
-api.add_resource(Movies_List, '/<string:movie>')
+api.add_resource(AllMovies, '/movies')
+api.add_resource(MoviesList, '/<string:movie>')
 
 if __name__ == '__main__':
 
---
+++ movies.py
@@ -0,0 +1,40 @@
+from base import Movies, db
+from flask_restful import Resource, reqparse, Api
+
+class MoviesList(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument('director', type=str, required=False, help='Director of the movie')
+    parser.add_argument('genre', type=str, required=False, help='Genre of the movie')
+    parser.add_argument('collection', type=int, required=True, help='Gross collection of the movie')
+
+    def get(self, movie):
+        item = Movies.find_by_title(movie)
+        if item:
+            return item.json()
+        return {'Message': 'Movie is not found'}
+
+    def post(self, movie):
+        if Movies.find_by_title(movie):
+            return {' Message': 'Movie with the  title {} already exists'.format(movie)}
+        args = Movies_List.parser.parse_args()
+        item = Movies(movie, args['director'], args['genre'], args['collection'])
+        item.save_to()
+        return item.json()
+
+    def put(self, movie):
+        args = Movies_List.parser.parse_args()
+        item = Movies.find_by_title(movie)
+        if item:
+            item.collection = args['collection']
+            item.save_to()
+            return {'Movie': item.json()}
+        item = Movies(movie, args['director'], args['genre'], args['collection'])
+        item.save_to()
+        return item.json()
+
+    def delete(self, movie):
+        item  = Movies.find_by_title(movie)
+        if item:
+            item.delete_()
+            return {'Message': '{} has been deleted from records'.format(movie)}
+        return {'Message': '{} is already not on the list'.format()}
